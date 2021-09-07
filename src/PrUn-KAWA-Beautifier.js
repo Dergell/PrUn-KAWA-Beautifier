52d624ce3e6e85fd973e4648e492214f28b377af
Tony Schmich <tony.schmich@7thsense.de>
2021-09-07 19:39:40 +0200

    v1.1 - added finish time to production queue - fixed long fractions in prices to 2 digits - overhaul of LMP changes / added price per unit

--- src/PrUn-KAWA-Beautifier.js
+++ src/PrUn-KAWA-Beautifier.js
@@ -8,6 +8,7 @@
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js
 // @require     https://cdn.jsdelivr.net/gh/timthedevguy/apexutils@0.0.35/src/apexutils.js
+// @require     https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js
 // @downloadURL https://raw.githubusercontent.com/Dergell/PrUn-KAWA-Beautifier/master/src/PrUn-KAWA-Beautifier.js
 // @updateURL   https://raw.githubusercontent.com/Dergell/PrUn-KAWA-Beautifier/master/src/PrUn-KAWA-Beautifier.js
 // ==/UserScript==
@@ -50,7 +51,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         // each LM ads row
         $('._14L--Z4VrwQHE-Dayta1db').each(function () {
             let row = $(this).find('._1owHJs3IjU2hxdT0zQ1ytB');
-            let line = row.html().split(" ");
+            let line = row.html().split(' ');
 
             // add done class to prevent recursion
             if (!row.hasClass('done')) {
@@ -58,7 +59,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                     let priceIndex = $.inArray('@', line) + 1;
                     let price = parseFloat(line[priceIndex].replace(',', '').replace('<span>', ''));
                     let quantity = parseInt(line[1]);
-                    let result = ' (' + price / quantity + '/u)';
+                    let result = ' (' + parseFloat((price / quantity).toFixed(2)) + '/u)';
 
                     line[priceIndex] = line[priceIndex] + result;
                     line[0] = '<span class="_1rsxmEXgWQAbBHGrAT6ZiI">' + line[0] + '</span>';   // class for green text color
@@ -66,7 +67,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                     let priceIndex = $.inArray('@', line) + 1;
                     let price = parseFloat(line[priceIndex].replace(',', '').replace('<span>', ''));
                     let quantity = parseInt(line[1]);
-                    let result = ' (' + price / quantity + '/u)';
+                    let result = ' (' + parseFloat((price / quantity).toFixed(2)) + '/u)';
 
                     line[priceIndex] = line[priceIndex] + result;
                     line[0] = '<span class="_37rC6F3tFhQffBlJ3pVE0N">' + line[0] + '</span>';   // class for red text color
@@ -81,22 +82,89 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
     // LMP -> Local Market Post
     function update_LMP() {
-        let type = $('form._3s_jDJ1t-xQhEfezpWyvsh ._3ghhLiZ1ioM5uEL9rCkYz6').first().find('._1XdRW9HaFHplJXb_FL0L4b');
-        let button = $('form._3s_jDJ1t-xQhEfezpWyvsh ._7iA4PlbA4_YVUCJRZ1RuQ ._1Y9l3J20Xn-CyxMZIcH06i');
+        let data = {};
 
-        if (type.text() == "BUYING") {
-            button.addClass('_3yZx55zAhax66rAfv6d6Z1'); // class for green button color
-            button.text('BUY');
-        } else if (type.text() == "SELLING") {
-            button.addClass('_31dQZugJBAqjKvME7bRBlA'); // class for red button color
-            button.text('SELL');
+        $('form._3s_jDJ1t-xQhEfezpWyvsh > div').each(function () {
+            let rowLabel = $(this).find('> label > span').text();
+            let rowContent = $(this).find('> div');
+
+            switch (rowLabel) {
+                case 'Type':
+                    data.adType = rowContent.text();
+                    break;
+                case 'Amount':
+                    data.adAmount = rowContent.find('input');
+                    break;
+                case 'Total price':
+                    data.adPrice = rowContent.find('input');
+                    if (!data.adPrice.prev().hasClass('unitPrice')) {
+                        data.adPrice.parent().prepend('<span class="unitPrice"></span>');
+                    }
+                    break;
+                case 'CMD':
+                    data.adButton = rowContent.find('button');
+                    break;
+            }
+        });
+
+        if (!$.isEmptyObject(data)) {
+            if (data.adType == "BUYING") {
+                data.adButton.addClass('_3yZx55zAhax66rAfv6d6Z1'); // class for green button color
+                data.adButton.text('BUY');
+            } else if (data.adType == "SELLING") {
+                data.adButton.addClass('_31dQZugJBAqjKvME7bRBlA'); // class for red button color
+                data.adButton.text('SELL');
+            }
+
+            if (data.adPrice.val() && data.adAmount.val()) {
+                data.adPrice.prev().text(parseFloat((data.adPrice.val() / data.adAmount.val()).toFixed(2)) + '/u ');
+            }
         }
+    }
+
+    // PRODQ -> Production Queue
+    function update_PRODQ() {
+        let data = {};
+
+        $('.B5JEuqpNoN-VT8jmA8g3l td:nth-child(5)').each(function () {
+            let span = $(this).find('span');
+
+            if (span.siblings('div').length == 0 && span.text().indexOf('in') != -1) {
+                let date = moment();
+                let timer = /((\d+)\s?([a-z]+)\s?)?(\d+)\s?([a-z]+)/.exec(span.text());
+
+                if (timer) {
+                    let daysIndex = $.inArray('days', timer);
+                    if (daysIndex) {
+                        date.add(timer[daysIndex - 1], timer[daysIndex]);
+                    }
+
+                    let hoursIndex = $.inArray('h', timer);
+                    if (hoursIndex) {
+                        date.add(timer[hoursIndex - 1], timer[hoursIndex]);
+                    }
+
+                    let minutesIndex = $.inArray('m', timer);
+                    if (minutesIndex) {
+                        date.add(timer[minutesIndex - 1], timer[minutesIndex]);
+                    }
+
+                    let secondsIndex = $.inArray('s', timer);
+                    if (secondsIndex) {
+                        date.add(timer[secondsIndex - 1], timer[secondsIndex]);
+                    }
+
+                    span.after('<div>' + date.calendar() + '</div>');
+                }
+            }
+        });
     }
 
     // trigger global update
     function update_beautifier() {
         update_LMP();
         update_LM();
+        update_PRODQ();
     }
 
 })();
